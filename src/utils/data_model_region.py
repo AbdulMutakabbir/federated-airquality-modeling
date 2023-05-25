d5fbb44cd6a62b3717792d43aed819e3a64e6c5f
AbdulMutakabbir <a_mutakabbir@yahoo.com>
2023-05-25 01:17:45 -0400

    added region enumerator

---
+++ src/utils/data_model_region.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class Region(Enum):
+    AOTIZHONGXIN = 'Aotizhongxin'
+    CHANGPING = 'Changping'
+    DINGLING = 'Dingling'
+    DONGSI = 'Dongsi'
+    GUANYUAN = 'Guanyuan'
+    GUCHENG = 'Gucheng'
+    HUAIROU = 'Huairou'
+    NONGZHANGUAN = 'Nongzhanguan'
+    SHUNYI = 'Shunyi'
+    TIANTAN = 'Tiantan'
+    WANLIU = 'Wanliu'
+    WANSHOUXIGONG = 'Wanshouxigong'
+
